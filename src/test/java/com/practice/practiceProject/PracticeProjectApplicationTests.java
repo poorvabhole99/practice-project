b15a4606e7708637016f90d5e7a7530af3dd496e
saurabh-ssingh <target4ever101@gmail.com>
2024-06-25 18:41:04 +0530

    removed unused code

--- src/test/java/com/practice/practiceProject/PracticeProjectApplicationTests.java
+++ src/test/java/com/practice/practiceProject/PracticeProjectApplicationTests.java
@@ -1,13 +1,7 @@
 package com.practice.practiceProject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PracticeProjectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
