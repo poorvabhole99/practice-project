a70607dd2c6b958054b0b6a870d1f88dc7a27e73
saurabh-ssingh <target4ever101@gmail.com>
2024-06-27 22:35:02 +0530

    implemented hikari cp

---
+++ src/main/java/com/practice/practiceProject/config/DataSourceConfig.java
@@ -0,0 +1,56 @@
+package com.practice.practiceProject.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    @Value("${spring.datasource.hikari.maximum-pool-size}")
+    private int maxPoolSize;
+
+    @Value("${spring.datasource.hikari.minimum-idle}")
+    private int minIdle;
+
+    @Value("${spring.datasource.hikari.connection-timeout}")
+    private long connectionTimeout;
+
+    @Value("${spring.datasource.hikari.idle-timeout}")
+    private long idleTimeout;
+
+    @Value("${spring.datasource.hikari.max-lifetime}")
+    private long maxLifetime;
+
+    @Value("${spring.datasource.hikari.pool-name}")
+    private String poolName;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(dbUrl);
+        hikariConfig.setUsername(dbUsername);
+        hikariConfig.setPassword(dbPassword);
+        hikariConfig.setMaximumPoolSize(maxPoolSize);
+        hikariConfig.setMinimumIdle(minIdle);
+        hikariConfig.setConnectionTimeout(connectionTimeout);
+        hikariConfig.setIdleTimeout(idleTimeout);
+        hikariConfig.setMaxLifetime(maxLifetime);
+        hikariConfig.setPoolName(poolName);
+
+        return new HikariDataSource(hikariConfig);
+    }
+}
