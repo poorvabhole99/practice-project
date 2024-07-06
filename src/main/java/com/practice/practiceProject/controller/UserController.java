4e4fb92163ab81354493ed82e0e711c776a24cd8
saurabh-ssingh <target4ever101@gmail.com>
2024-07-06 12:54:04 +0530

    implemented spring boot actuator

--- src/main/java/com/practice/practiceProject/controller/UserController.java
+++ src/main/java/com/practice/practiceProject/controller/UserController.java
@@ -26,49 +26,68 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 @Log4j2
 public class UserController {
-    @Autowired
-    private UserService userService;
 
-    @PostMapping("/create")
-    @TrackExecutionTime
-    public ResponseEntity<PracticeProjectResponse> createUser(@Valid @RequestBody  User user) throws PracticeProjectException {
-        log.info("Started create user API ");
-        final PracticeProjectResponse practiceProjectResponse = this.userService.createUser(user);
-        log.info("Completed create user API");
-        return new ResponseEntity<>(practiceProjectResponse, HttpStatus.CREATED);
-    }
+  @Autowired
+  private UserService userService;
 
-    @GetMapping("/getUser/{emailId}")
-    @TrackExecutionTime
-    public ResponseEntity<PracticeProjectResponse> getSingleUser(@PathVariable String emailId) throws UserNotFoundException {
-        log.info("Started get single user API, emailId :{}",emailId);
-        final PracticeProjectResponse practiceProjectResponse = this.userService.getSingleUser(emailId);
-        log.info("Completed get single user API, emailId :{}",emailId);
-        return new ResponseEntity<>(practiceProjectResponse, HttpStatus.OK);
-    }
+  /**
+   * Create a new user.
+   *
+   * @param user the user to be created
+   * @return the response entity containing the newly created user
+   * @throws PracticeProjectException if an error occurs during user creation
+   * @author: Poorva Bhole (implementation)
+   */
+  @PostMapping("/create")
+  @TrackExecutionTime
+  public ResponseEntity<PracticeProjectResponse> createUser(@Valid @RequestBody User user)
+      throws PracticeProjectException {
+    final PracticeProjectResponse practiceProjectResponse = this.userService.createUser(user);
+    return new ResponseEntity<>(practiceProjectResponse, HttpStatus.CREATED);
+  }
 
-    @DeleteMapping("/delete/{emailId}")
-    @TrackExecutionTime
-    public ResponseEntity<PracticeProjectResponse> deleteUser(@PathVariable String emailId) throws UserNotFoundException {
-        final PracticeProjectResponse practiceProjectResponse = this.userService.deleteUser(emailId);
-        log.info("Completed delete user API");
-        return new ResponseEntity<>(practiceProjectResponse, HttpStatus.OK);
-    }
+  /**
+   * Retrieves a single user based on their email ID.
+   *
+   * @param emailId the unique email ID of the user to retrieve
+   * @return a ResponseEntity containing the response with details of the retrieved user
+   * @throws UserNotFoundException if the user with the specified email ID does not exist
+   * @author Poorva Bhole (implementation)
+   */
+  @GetMapping("/getUser/{emailId}")
+  @TrackExecutionTime
+  public ResponseEntity<PracticeProjectResponse> getSingleUser(@PathVariable String emailId)
+      throws UserNotFoundException {
+    final PracticeProjectResponse practiceProjectResponse = this.userService.getSingleUser(emailId);
+    return new ResponseEntity<>(practiceProjectResponse, HttpStatus.OK);
+  }
 
-    @PutMapping("/update/{emailId}")
-    @TrackExecutionTime
-    public ResponseEntity<PracticeProjectResponse> updateUser(@PathVariable String emailId,@Valid @RequestBody UserInputDto userDto) throws PracticeProjectException {
-        final PracticeProjectResponse practiceProjectResponse = this.userService.updateUser(emailId, userDto);
-        return new ResponseEntity<>(practiceProjectResponse, HttpStatus.OK);
-    }
+  @DeleteMapping("/delete/{emailId}")
+  @TrackExecutionTime
+  public ResponseEntity<PracticeProjectResponse> deleteUser(@PathVariable String emailId)
+      throws UserNotFoundException {
+    final PracticeProjectResponse practiceProjectResponse = this.userService.deleteUser(emailId);
+    log.info("Completed delete user API");
+    return new ResponseEntity<>(practiceProjectResponse, HttpStatus.OK);
+  }
 
-    @PutMapping("/activate/{emailId}")
-    @TrackExecutionTime
-    public ResponseEntity<PracticeProjectResponse> activateUser(@PathVariable String emailId) throws UserNotFoundException {
-        final PracticeProjectResponse practiceProjectResponse = this.userService.activateUser(emailId);
-        log.info("Completed activate user API");
-        return new ResponseEntity<>(practiceProjectResponse, HttpStatus.OK);
-    }
+  @PutMapping("/update/{emailId}")
+  @TrackExecutionTime
+  public ResponseEntity<PracticeProjectResponse> updateUser(@PathVariable String emailId,
+      @Valid @RequestBody UserInputDto userDto) throws PracticeProjectException {
+    final PracticeProjectResponse practiceProjectResponse = this.userService.updateUser(emailId,
+        userDto);
+    return new ResponseEntity<>(practiceProjectResponse, HttpStatus.OK);
+  }
+
+  @PutMapping("/activate/{emailId}")
+  @TrackExecutionTime
+  public ResponseEntity<PracticeProjectResponse> activateUser(@PathVariable String emailId)
+      throws UserNotFoundException {
+    final PracticeProjectResponse practiceProjectResponse = this.userService.activateUser(emailId);
+    log.info("Completed activate user API");
+    return new ResponseEntity<>(practiceProjectResponse, HttpStatus.OK);
+  }
 
 
 }
