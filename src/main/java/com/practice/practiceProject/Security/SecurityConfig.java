8e777e9351e833a376e94ffb4d2cc9c9a9caf1a0
Saurabh Singh <target4ever101@gmail.com>
2024-07-08 11:23:25 +0530

    implemented spring boot actuator (#28)

--- src/main/java/com/practice/practiceProject/Security/SecurityConfig.java
+++ src/main/java/com/practice/practiceProject/Security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.practice.practiceProject.Security;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -21,106 +19,109 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-    private final UserDetailsService userDetailsService;
-    private final JwtAuthenticationEntryPoint point;
-    private final JwtAuthFilter jwtAuthFilter;
-    private final JWTAccessDeniedHandler jwtAccessDeniedHandler;
 
-    public SecurityConfig(JwtAuthFilter jwtAuthFilter, UserDetailsService userDetailsService, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,JWTAccessDeniedHandler jwtAccessDeniedHandler) {
-        this.jwtAuthFilter = jwtAuthFilter;
-        this.userDetailsService = userDetailsService;
-        this.point = jwtAuthenticationEntryPoint;
-        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
-    }
+  private static final String EMPLOYEE = "EMPLOYEE";
+  private static final String ADMIN = "ADMIN";
+  private final UserDetailsService userDetailsService;
+  private final JwtAuthenticationEntryPoint point;
+  private final JwtAuthFilter jwtAuthFilter;
+  private final JWTAccessDeniedHandler jwtAccessDeniedHandler;
+  public SecurityConfig(JwtAuthFilter jwtAuthFilter, UserDetailsService userDetailsService,
+      JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+      JWTAccessDeniedHandler jwtAccessDeniedHandler) {
+    this.jwtAuthFilter = jwtAuthFilter;
+    this.userDetailsService = userDetailsService;
+    this.point = jwtAuthenticationEntryPoint;
+    this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+  }
 
-    private static final String EMPLOYEE = "EMPLOYEE";
-    private static final String ADMIN = "ADMIN";
+  /**
+   * Configuration method for defining a SecurityFilterChain bean. This bean configures the security
+   * filters to be applied to HTTP requests, specifying authentication, authorization rules, and
+   * exception handling.
+   *
+   * @param http The HttpSecurity object provided by Spring Security for configuring security
+   *             settings.
+   * @return A SecurityFilterChain configured with specified security settings for HTTP requests.
+   * @throws Exception If an error occurs during configuration.
+   */
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // Disable CSRF protection and CORS (Cross-Origin Resource Sharing) to simplify configuration
+    http.csrf(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth ->
+            auth
+                .requestMatchers("/actuator/**").permitAll()
+                // Permit access to "/user/create" endpoint without authentication
+                .requestMatchers("/user/create").permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                // Require authentication for endpoints matching "/user/getUser/**"
+                .requestMatchers(HttpMethod.GET, "/user/getUser/**").hasAnyRole(ADMIN, EMPLOYEE)
+                .requestMatchers(HttpMethod.PUT, "/user/update/**").hasAnyRole(ADMIN)
+                .requestMatchers(HttpMethod.PUT, "/user/activate/**").hasAnyRole(ADMIN, EMPLOYEE)
+                .requestMatchers("/user/delete/**").hasRole(ADMIN)
+                .requestMatchers("/test/**").permitAll())
 
-
-    /**
-     * Configuration method for defining a SecurityFilterChain bean.
-     * This bean configures the security filters to be applied to HTTP requests,
-     * specifying authentication, authorization rules, and exception handling.
-     *
-     * @param http The HttpSecurity object provided by Spring Security for configuring security settings.
-     * @return A SecurityFilterChain configured with specified security settings for HTTP requests.
-     * @throws Exception If an error occurs during configuration.
-     */
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // Disable CSRF protection and CORS (Cross-Origin Resource Sharing) to simplify configuration
-        http.csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth ->
-                        auth
-                                // Permit access to "/user/create" endpoint without authentication
-                                .requestMatchers("/user/create").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                                // Require authentication for endpoints matching "/user/getUser/**"
-                                .requestMatchers(HttpMethod.GET, "/user/getUser/**").hasAnyRole(ADMIN, EMPLOYEE)
-                                .requestMatchers(HttpMethod.PUT,"/user/update/**").hasAnyRole(ADMIN)
-                                .requestMatchers(HttpMethod.PUT, "/user/activate/**").hasAnyRole(ADMIN, EMPLOYEE)
-                                .requestMatchers("/user/delete/**").hasRole(ADMIN)
-                                .requestMatchers("/test/**").permitAll())
-
-                // Configure authentication provider to be used for authentication
-                .authenticationProvider(daoAuthenticationProvider())
-                // Use HTTP Basic authentication with default settings
+        // Configure authentication provider to be used for authentication
+        .authenticationProvider(daoAuthenticationProvider())
+        // Use HTTP Basic authentication with default settings
 //        .httpBasic(withDefaults())
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                // Configure exception handling for authentication failures
-                .exceptionHandling(ex -> ex.authenticationEntryPoint(point))
-                .exceptionHandling(ex-> ex.accessDeniedHandler(jwtAccessDeniedHandler));
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        // Configure exception handling for authentication failures
+        .exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+        .exceptionHandling(ex -> ex.accessDeniedHandler(jwtAccessDeniedHandler));
 
-        // Build and return the configured HttpSecurity object as a SecurityFilterChain
-        return http.build();
-    }
-
-
-    /**
-     * Configuration method for providing a bean of type PasswordEncoder.
-     * This bean is responsible for encoding passwords using the BCrypt hashing algorithm,
-     * which is a widely used and secure method for password hashing.
-     *
-     * @return A BCryptPasswordEncoder instance, which implements the PasswordEncoder interface.
-     * This encoder can be used to securely hash passwords before storing them in a database
-     * or comparing them with hashed passwords during authentication.
-     */
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        // Return a new instance of BCryptPasswordEncoder, which implements the PasswordEncoder interface
-        return new BCryptPasswordEncoder();
-    }
+    // Build and return the configured HttpSecurity object as a SecurityFilterChain
+    return http.build();
+  }
 
 
-    /**
-     * Configuration method for providing an AuthenticationProvider bean.
-     * This bean configures a DaoAuthenticationProvider, which is responsible for authenticating users
-     * based on information retrieved from a UserDetailsService and using a PasswordEncoder for password validation.
-     *
-     * @return An instance of AuthenticationProvider, specifically a DaoAuthenticationProvider,
-     * configured with a UserDetailsService and a PasswordEncoder.
-     * This provider can be used in Spring Security configuration to authenticate users
-     * based on credentials stored in a database or other data source.
-     */
-    @Bean
-    public AuthenticationProvider daoAuthenticationProvider() {
-        // Create a new instance of DaoAuthenticationProvider
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+  /**
+   * Configuration method for providing a bean of type PasswordEncoder. This bean is responsible for
+   * encoding passwords using the BCrypt hashing algorithm, which is a widely used and secure method
+   * for password hashing.
+   *
+   * @return A BCryptPasswordEncoder instance, which implements the PasswordEncoder interface. This
+   * encoder can be used to securely hash passwords before storing them in a database or comparing
+   * them with hashed passwords during authentication.
+   */
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    // Return a new instance of BCryptPasswordEncoder, which implements the PasswordEncoder interface
+    return new BCryptPasswordEncoder();
+  }
 
-        // Set the UserDetailsService to be used by the provider for retrieving user details
-        provider.setUserDetailsService(userDetailsService);
 
-        // Set the PasswordEncoder to be used by the provider for password validation
-        provider.setPasswordEncoder(passwordEncoder());
+  /**
+   * Configuration method for providing an AuthenticationProvider bean. This bean configures a
+   * DaoAuthenticationProvider, which is responsible for authenticating users based on information
+   * retrieved from a UserDetailsService and using a PasswordEncoder for password validation.
+   *
+   * @return An instance of AuthenticationProvider, specifically a DaoAuthenticationProvider,
+   * configured with a UserDetailsService and a PasswordEncoder. This provider can be used in Spring
+   * Security configuration to authenticate users based on credentials stored in a database or other
+   * data source.
+   */
+  @Bean
+  public AuthenticationProvider daoAuthenticationProvider() {
+    // Create a new instance of DaoAuthenticationProvider
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 
-        // Return the configured DaoAuthenticationProvider
-        return provider;
-    }
+    // Set the UserDetailsService to be used by the provider for retrieving user details
+    provider.setUserDetailsService(userDetailsService);
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+    // Set the PasswordEncoder to be used by the provider for password validation
+    provider.setPasswordEncoder(passwordEncoder());
+
+    // Return the configured DaoAuthenticationProvider
+    return provider;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
 
 }
--- src/main/java/com/practice/practiceProject/audit/SpringSecurityAuditorAwareImpl.java
+++ src/main/java/com/practice/practiceProject/audit/SpringSecurityAuditorAwareImpl.java
@@ -1,81 +1,45 @@
 package com.practice.practiceProject.audit;
 
 import java.util.Optional;
-
-
 import org.springframework.data.domain.AuditorAware;
-
-
 import org.springframework.security.core.Authentication;
-
-
 import org.springframework.security.core.context.SecurityContextHolder;
 
 
 /**
- * Implementation of the Spring Security AuditorAware interface
- * <p>
- * <p>
- * for obtaining the current auditor (user) responsible for auditing actions.
- * <p>
- * <p>
- * This class retrieves the username of the currently authenticated user
- * <p>
- * <p>
- * from the Spring Security context.
+ * Implementation of the Spring Security AuditorAware interface for obtaining the current auditor
+ * (user) responsible for auditing actions. This class retrieves the username of the currently
+ * authenticated user from the Spring Security context.
  */
 
 
 public class SpringSecurityAuditorAwareImpl implements AuditorAware<String> {
 
 
-    /**
-     * Retrieves an Optional containing the username of the currently authenticated user,
-     * <p>
-     * <p>
-     * if available, from the Spring Security context.
-     *
-     * @return An Optional containing the username of the current auditor,
-     * <p>
-     * <p>
-     * or an empty Optional if no authenticated user is found.
-     */
+  /**
+   * Retrieves an Optional containing the username of the currently authenticated user, if
+   * available, from the Spring Security context.
+   *
+   * @return An Optional containing the username of the current auditor, or an empty Optional if no
+   * authenticated user is found.
+   */
 
 
-    @Override
-    public Optional<String> getCurrentAuditor() {
-        // Retrieve the current authentication object from the Spring Security context
+  @Override
+  public Optional<String> getCurrentAuditor() {
+    // Retrieve the current authentication object from the Spring Security context
+    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-
-        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println(authentication.getPrincipal().toString());
-
-        // Check if authentication is null or if the user is not authenticated
-
-        if (authentication == null || !authentication.isAuthenticated()) {
-
-            // No authenticated user found, return an empty Optional
-
-
-            return Optional.empty();
-
-
-        }
-
-
-        // Retrieve the username of the authenticated user
-
-
-        final String username = authentication.getName();
-
-
-        // Return an Optional containing the username of the current auditor
-
-
-        return Optional.of(username);
-
-
+    // Check if authentication is null or if the user is not authenticated
+    if (authentication == null || !authentication.isAuthenticated()) {
+      // No authenticated user found, return an empty Optional
+      return Optional.empty();
     }
 
+    // Retrieve the username of the authenticated user
+    final String username = authentication.getName();
 
+    // Return an Optional containing the username of the current auditor
+    return Optional.of(username);
+  }
 }
